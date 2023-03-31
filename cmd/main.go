cd8ab2903fec0c0576c513d3168ff8ce957a0dab
Henrik Larsson <henriklarsson@Henriks-MacBook-Pro.local>
2023-03-31 23:07:03 +0200

    Fix error in action

--- cmd/main.go
+++ cmd/main.go
@@ -7,14 +7,9 @@ import (
 )
 
 func main() {
-
 	addr := ":8080"
-
 	mux := http.NewServeMux()
-
 	mux.HandleFunc("/hellooo", rest.TranslateHandler)
-
 	log.Printf("listening on %s\n", addr)
-
 	log.Fatal(http.ListenAndServe(addr, mux))
 }
